7fe4435737779c4e4c6277ea0da43c9f65b774d7
HelloWorldZTR <2279429006@qq.com>
2025-10-24 09:10:08 +0000

    Update bubble.ys

--- lab4/archlab-project/misc/examples.s
+++ lab4/archlab-project/misc/examples.s
@@ -6,27 +6,20 @@ sum_list:
 .LFB0:
 	.cfi_startproc
 	endbr64
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	movq	%rdi, -24(%rbp)
-	movq	$0, -8(%rbp)
+	movq	%rdi, -24(%rsp)
+	movq	$0, -8(%rsp)
 	jmp	.L2
 .L3:
-	movq	-24(%rbp), %rax
+	movq	-24(%rsp), %rax
 	movq	(%rax), %rax
-	addq	%rax, -8(%rbp)
-	movq	-24(%rbp), %rax
+	addq	%rax, -8(%rsp)
+	movq	-24(%rsp), %rax
 	movq	8(%rax), %rax
-	movq	%rax, -24(%rbp)
+	movq	%rax, -24(%rsp)
 .L2:
-	cmpq	$0, -24(%rbp)
+	cmpq	$0, -24(%rsp)
 	jne	.L3
-	movq	-8(%rbp), %rax
-	popq	%rbp
-	.cfi_def_cfa 7, 8
+	movq	-8(%rsp), %rax
 	ret
 	.cfi_endproc
 .LFE0:
@@ -37,32 +30,28 @@ rsum_list:
 .LFB1:
 	.cfi_startproc
 	endbr64
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	subq	$32, %rsp
-	movq	%rdi, -24(%rbp)
-	cmpq	$0, -24(%rbp)
+	subq	$40, %rsp
+	.cfi_def_cfa_offset 48
+	movq	%rdi, 8(%rsp)
+	cmpq	$0, 8(%rsp)
 	jne	.L6
 	movl	$0, %eax
 	jmp	.L7
 .L6:
-	movq	-24(%rbp), %rax
+	movq	8(%rsp), %rax
 	movq	(%rax), %rax
-	movq	%rax, -16(%rbp)
-	movq	-24(%rbp), %rax
+	movq	%rax, 16(%rsp)
+	movq	8(%rsp), %rax
 	movq	8(%rax), %rax
 	movq	%rax, %rdi
 	call	rsum_list
-	movq	%rax, -8(%rbp)
-	movq	-16(%rbp), %rdx
-	movq	-8(%rbp), %rax
+	movq	%rax, 24(%rsp)
+	movq	16(%rsp), %rdx
+	movq	24(%rsp), %rax
 	addq	%rdx, %rax
 .L7:
-	leave
-	.cfi_def_cfa 7, 8
+	addq	$40, %rsp
+	.cfi_def_cfa_offset 8
 	ret
 	.cfi_endproc
 .LFE1:
@@ -73,62 +62,69 @@ bubble_sort:
 .LFB2:
 	.cfi_startproc
 	endbr64
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	movq	%rdi, -40(%rbp)
-	movq	%rsi, -48(%rbp)
-	movq	-48(%rbp), %rax
+	movq	%rdi, -40(%rsp)
+	movq	%rsi, -48(%rsp)
+	movq	-48(%rsp), %rax
 	salq	$3, %rax
 	leaq	-8(%rax), %rdx
-	movq	-40(%rbp), %rax
+	movq	-40(%rsp), %rax
 	addq	%rdx, %rax
-	movq	%rax, -16(%rbp)
+	movq	%rax, -16(%rsp)
 	jmp	.L9
 .L13:
-	movq	-40(%rbp), %rax
-	movq	%rax, -24(%rbp)
+	movq	-40(%rsp), %rax
+	movq	%rax, -24(%rsp)
 	jmp	.L10
 .L12:
-	movq	-24(%rbp), %rax
+	movq	-24(%rsp), %rax
 	addq	$8, %rax
 	movq	(%rax), %rdx
-	movq	-24(%rbp), %rax
+	movq	-24(%rsp), %rax
 	movq	(%rax), %rax
 	cmpq	%rax, %rdx
 	jge	.L11
-	movq	-24(%rbp), %rax
+	movq	-24(%rsp), %rax
 	movq	8(%rax), %rax
-	movq	%rax, -8(%rbp)
-	movq	-24(%rbp), %rax
+	movq	%rax, -8(%rsp)
+	movq	-24(%rsp), %rax
 	leaq	8(%rax), %rdx
-	movq	-24(%rbp), %rax
+	movq	-24(%rsp), %rax
 	movq	(%rax), %rax
 	movq	%rax, (%rdx)
-	movq	-24(%rbp), %rax
-	movq	-8(%rbp), %rdx
+	movq	-24(%rsp), %rax
+	movq	-8(%rsp), %rdx
 	movq	%rdx, (%rax)
 .L11:
-	addq	$8, -24(%rbp)
+	addq	$8, -24(%rsp)
 .L10:
-	movq	-24(%rbp), %rax
-	cmpq	-16(%rbp), %rax
+	movq	-24(%rsp), %rax
+	cmpq	-16(%rsp), %rax
 	jb	.L12
-	subq	$8, -16(%rbp)
+	subq	$8, -16(%rsp)
 .L9:
-	movq	-16(%rbp), %rax
-	cmpq	%rax, -40(%rbp)
+	movq	-16(%rsp), %rax
+	cmpq	%rax, -40(%rsp)
 	jb	.L13
 	nop
 	nop
-	popq	%rbp
-	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
 .LFE2:
 	.size	bubble_sort, .-bubble_sort
+	.globl	main
+	.type	main, @function
+main:
+.LFB3:
+	.cfi_startproc
+	endbr64
+	movl	$0, %esi
+	movl	$0, %edi
+	call	bubble_sort
+	movl	$0, %eax
+	ret
+	.cfi_endproc
+.LFE3:
+	.size	main, .-main
 	.ident	"GCC: (Ubuntu 13.3.0-6ubuntu2~24.04) 13.3.0"
 	.section	.note.GNU-stack,"",@progbits
 	.section	.note.gnu.property,"a"
