ed962f251bdad79b135d9bbe234700feec2606b7
HelloWorldZTR <2279429006@qq.com>
2025-11-07 14:11:02 +0000

    Lab 5 better

--- lab5/trans.c
+++ lab5/trans.c
@@ -32,28 +32,99 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
     int j;
     int x;
     int y;
-
-    int blockSize = 16;
+    int t0, t1, t2, t3, t4, t5, t6, t7;
 
     if (M == 32 && N == 32) {
-        blockSize = 8;
-    } else if (M == 64 && N == 64) {
-        blockSize = 4;
-    } else {
-        blockSize = 8;
-    }
-
-    for (i = 0; i < N; i += blockSize) {
-        for (j = 0; j < M; j += blockSize) {
-            // block from A[i][j] to A[i+15][j+15]
-            for (x = i; x < i + blockSize && x < N; x++) {
-                for (y = j; y < j + blockSize && y < M; y++) {
-                    if (x != y) {
-                        B[y][x] = A[x][y];
+        // block size 8
+        for (i = 0; i < N; i += 8) {
+            for (j = 0; j < M; j += 8) {
+                // block from A[i][j] to A[i+15][j+15]
+                for (x = i; x < i + 8 && x < N; x++) {
+                    for (y = j; y < j + 8 && y < M; y++) {
+                        if (x != y) {
+                            B[y][x] = A[x][y];
+                        }
+                    }
+                    if (i == j) {
+                        B[x][x] = A[x][x];
                     }
                 }
-                if (i == j) {
-                    B[x][x] = A[x][x];
+            }
+        }
+    } else if (M == 64 && N == 64) {
+        // block size 8 + 4 x 4 row optimize
+        for (i = 0; i < N; i += 8) {
+            for (j = 0; j < M; j += 8) {
+
+                for (x = i; x < i + 4; x++) {
+                    t0 = A[x][j + 0];
+                    t1 = A[x][j + 1];
+                    t2 = A[x][j + 2];
+                    t3 = A[x][j + 3];
+                    t4 = A[x][j + 4];
+                    t5 = A[x][j + 5];
+                    t6 = A[x][j + 6];
+                    t7 = A[x][j + 7];
+
+                    B[j + 0][x] = t0;
+                    B[j + 1][x] = t1;
+                    B[j + 2][x] = t2;
+                    B[j + 3][x] = t3;
+                    B[j + 0][x + 4] = t4;
+                    B[j + 1][x + 4] = t5;
+                    B[j + 2][x + 4] = t6;
+                    B[j + 3][x + 4] = t7;
+                }
+                // Do it row-wise to minimize cache miss
+                for (y = j + 4; y < j + 8; y++) {
+                    t0 = B[y - 4][i + 4];
+                    t1 = B[y - 4][i + 5];
+                    t2 = B[y - 4][i + 6];
+                    t3 = B[y - 4][i + 7];
+
+                    t4 = A[i + 4][y - 4];
+                    t5 = A[i + 5][y - 4];
+                    t6 = A[i + 6][y - 4];
+                    t7 = A[i + 7][y - 4];
+
+                    B[y - 4][i + 4] = t4;
+                    B[y - 4][i + 5] = t5;
+                    B[y - 4][i + 6] = t6;
+                    B[y - 4][i + 7] = t7;
+
+                    B[y][i + 0] = t0;
+                    B[y][i + 1] = t1;
+                    B[y][i + 2] = t2;
+                    B[y][i + 3] = t3;
+                }
+
+                for (x = i + 4; x < i + 8; x++) {
+                    t0 = A[x][j + 4];
+                    t1 = A[x][j + 5];
+                    t2 = A[x][j + 6];
+                    t3 = A[x][j + 7];
+
+                    B[j + 4][x] = t0;
+                    B[j + 5][x] = t1;
+                    B[j + 6][x] = t2;
+                    B[j + 7][x] = t3;
+                }
+            }
+        }
+    } else {
+        // block size 8
+        for (i = 0; i < N; i += 8) {
+            for (j = 0; j < M; j += 8) {
+                // block from A[i][j] to A[i+15][j+15]
+                for (x = i; x < i + 8 && x < N; x++) {
+                    for (y = j; y < j + 8 && y < M; y++) {
+                        if (x != y) {
+                            B[y][x] = A[x][y];
+                        }
+                    }
+                    if (i == j) {
+                        B[x][x] = A[x][x];
+                    }
                 }
             }
         }
@@ -122,8 +193,67 @@ void transpose_block_8(int M, int N, int A[N][M], int B[M][N])
     ENSURES(is_transpose(M, N, A, B));
 }
 
-char transpose_block_8b_desc[] = "Transpose with block size 8 (desync)";
+
+
+char transpose_block_8b_desc[] = "Transpose with block size 4x2";
 void transpose_block_8b(int M, int N, int A[N][M], int B[M][N])
+{
+    REQUIRES(M > 0);
+    REQUIRES(N > 0);
+    //1kb = 32b * 32sets
+    int i;
+    int j;
+    // int x;
+    // int y;
+    int blockSize = 4;
+    for (i = 0; i < N / blockSize; i ++) {
+        for (j = 0; j < M / blockSize; j ++) {
+            int t00 = A[i*blockSize + 0][j*blockSize + 0];
+            int t01 = A[i*blockSize + 0][j*blockSize + 1];
+            int t02 = A[i*blockSize + 0][j*blockSize + 2];
+            int t03 = A[i*blockSize + 0][j*blockSize + 3];
+            int t10 = A[i*blockSize + 1][j*blockSize + 0];
+            int t11 = A[i*blockSize + 1][j*blockSize + 1];
+            int t12 = A[i*blockSize + 1][j*blockSize + 2];
+            int t13 = A[i*blockSize + 1][j*blockSize + 3];
+
+            B[j*blockSize + 0][i*blockSize + 0] = t00;
+            B[j*blockSize + 0][i*blockSize + 1] = t10;
+            B[j*blockSize + 1][i*blockSize + 0] = t01;
+            B[j*blockSize + 1][i*blockSize + 1] = t11;
+            B[j*blockSize + 2][i*blockSize + 0] = t02;
+            B[j*blockSize + 2][i*blockSize + 1] = t12;
+            B[j*blockSize + 3][i*blockSize + 0] = t03;
+            B[j*blockSize + 3][i*blockSize + 1] = t13;
+
+            t00 = A[i*blockSize + 2][j*blockSize + 0];
+            t01 = A[i*blockSize + 2][j*blockSize + 1];
+            t02 = A[i*blockSize + 2][j*blockSize + 2];
+            t03 = A[i*blockSize + 2][j*blockSize + 3];
+            t10 = A[i*blockSize + 3][j*blockSize + 0];
+            t11 = A[i*blockSize + 3][j*blockSize + 1];
+            t12 = A[i*blockSize + 3][j*blockSize + 2];
+            t13 = A[i*blockSize + 3][j*blockSize + 3];
+
+            B[j*blockSize + 0][i*blockSize + 2] = t00;
+            B[j*blockSize + 0][i*blockSize + 3] = t10;
+            B[j*blockSize + 1][i*blockSize + 2] = t01;
+            B[j*blockSize + 1][i*blockSize + 3] = t11;
+            B[j*blockSize + 2][i*blockSize + 2] = t02;
+            B[j*blockSize + 2][i*blockSize + 3] = t12;
+            B[j*blockSize + 3][i*blockSize + 2] = t03;
+            B[j*blockSize + 3][i*blockSize + 3] = t13;
+        }
+    }
+
+    ENSURES(is_transpose(M, N, A, B));
+}
+
+
+
+
+char transpose_block_8c_desc[] = "Transpose with block size (8x4)";
+void transpose_block_8c(int M, int N, int A[N][M], int B[M][N])
 {
     REQUIRES(M > 0);
     REQUIRES(N > 0);
@@ -132,24 +262,62 @@ void transpose_block_8b(int M, int N, int A[N][M], int B[M][N])
     int j;
     int x;
     int y;
-    int blockSize = 4;
-    for (i = 0; i < N; i += blockSize) {
-        for (j = 0; j < M; j += blockSize) {
-            if (i != j) {
-                for (x = i; x < i + blockSize && x < N; x++) {
-                    for (y = j; y < j + blockSize && y < M; y++) {
-                            B[y][x] = A[x][y];
-                    }
-                }
-            } else { // diagonal block
-                for (x = i; x < i + blockSize && x < N; x++) {
-                    for (y = j; y < j + blockSize && y < M; y++) {
-                        if (x != y) {
-                            B[y][x] = A[x][y];
-                        }
-                    }
-                    B[x][x] = A[x][x];
-                }
+    int t0, t1, t2, t3, t4, t5, t6, t7;
+    for (i = 0; i < N; i += 8) {
+        for (j = 0; j < M; j += 8) {
+
+            for (x = i; x < i + 4; x++) {
+                t0 = A[x][j + 0];
+                t1 = A[x][j + 1];
+                t2 = A[x][j + 2];
+                t3 = A[x][j + 3];
+                t4 = A[x][j + 4];
+                t5 = A[x][j + 5];
+                t6 = A[x][j + 6];
+                t7 = A[x][j + 7];
+
+                B[j + 0][x] = t0;
+                B[j + 1][x] = t1;
+                B[j + 2][x] = t2;
+                B[j + 3][x] = t3;
+                B[j + 0][x + 4] = t4;
+                B[j + 1][x + 4] = t5;
+                B[j + 2][x + 4] = t6;
+                B[j + 3][x + 4] = t7;
+            }
+            // Do it row-wise to minimize cache miss
+            for (y = j + 4; y < j + 8; y++) {
+                t0 = B[y - 4][i + 4];
+                t1 = B[y - 4][i + 5];
+                t2 = B[y - 4][i + 6];
+                t3 = B[y - 4][i + 7];
+
+                t4 = A[i + 4][y - 4];
+                t5 = A[i + 5][y - 4];
+                t6 = A[i + 6][y - 4];
+                t7 = A[i + 7][y - 4];
+
+                B[y - 4][i + 4] = t4;
+                B[y - 4][i + 5] = t5;
+                B[y - 4][i + 6] = t6;
+                B[y - 4][i + 7] = t7;
+
+                B[y][i + 0] = t0;
+                B[y][i + 1] = t1;
+                B[y][i + 2] = t2;
+                B[y][i + 3] = t3;
+            }
+
+            for (x = i + 4; x < i + 8; x++) {
+                t0 = A[x][j + 4];
+                t1 = A[x][j + 5];
+                t2 = A[x][j + 6];
+                t3 = A[x][j + 7];
+
+                B[j + 4][x] = t0;
+                B[j + 5][x] = t1;
+                B[j + 6][x] = t2;
+                B[j + 7][x] = t3;
             }
         }
     }
@@ -231,6 +399,7 @@ void registerFunctions()
     // registerTransFunction(transpose_block_8, transpose_block_8_desc);
     // registerTransFunction(transpose_block_16, transpose_block_16_desc);
     // registerTransFunction(transpose_block_8b, transpose_block_8b_desc);
+    // registerTransFunction(transpose_block_8c, transpose_block_8c_desc);
 }
 
 /*
